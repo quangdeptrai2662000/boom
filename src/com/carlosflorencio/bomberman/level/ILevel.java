baf01e2200b61ccf85649cafeceed137ba8de085
quangdeptrai2662000 <72980141+quangdeptrai2662000@users.noreply.github.com>
2020-12-18 08:34:10 +0700

    Delete ILevel.java

--- src/com/carlosflorencio/bomberman/level/ILevel.java
+++
@@ -1,9 +0,0 @@
-package com.carlosflorencio.bomberman.level;
-
-
-import com.carlosflorencio.bomberman.exceptions.LoadLevelException;
-
-public interface ILevel {
-
-	public void loadLevel(String path) throws LoadLevelException;
-}
